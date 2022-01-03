96166c17db2ea1a5e467a5b281b87809849078ba
dannomuggli <72226007+dannomuggli@users.noreply.github.com>
2022-01-02 21:38:00 -0800

    comments only

--- src/com/company/SolutionContainerWithMostWater.java
+++ src/com/company/SolutionContainerWithMostWater.java
@@ -18,18 +18,34 @@ public class SolutionContainerWithMostWater {
         int b_pointer = height.length - 1;
 
         // Now run a while loop with the condition that
-        // the a_pointer stays less than b_pointer
-        // they can cross or your area would be negative
+        // the a_pointer stays less than b_pointer (within the array)
+        // they can't cross or your area would be negative
 
         while (a_pointer < b_pointer) {
+
+            // if the height of column a_pointer is less than
+            // b_pointer column you'll run the calculation below
+            // and then move a_pointer one slot and recalculate
             if (height[a_pointer] < height[b_pointer]) {
+
+                // calculate the max area by taking the height of
+                // the a_pointer multiplied by the difference in the
+                // b & a pointers. Then you can increment the a_pointer
+                // so you move to the next bar in the graph. then you
+                // calculate the area and replace if you have a new max_area
                 max_area = Math.max(max_area, height[a_pointer] * (b_pointer - a_pointer));
                 a_pointer += 1;
             } else {
+
+                // if b_pointer is <= to a_pointer you'll run this calculation
+                // and then decrement b_pointer to the next column
+                // this happens until a_pointer == b_pointer
                 max_area = Math.max(max_area, height[b_pointer] * (b_pointer - a_pointer));
                 b_pointer -= 1;
             }
         }
+        // each time through the max_area is updated by the Math.max command
+        // so the largest result will be returned on this line.
         return max_area;
 
     }
--- src/com/company/SolutionGCD.java
+++ src/com/company/SolutionGCD.java
@@ -8,6 +8,13 @@ import java.util.Scanner;
 public class SolutionGCD
 {
     // METHOD SIGNATURE BEGINS, THIS METHOD IS REQUIRED
+
+    // You need to set up a main method to execute the
+    // method given to calculate the GCD
+
+    // The method is the Euclidian solution for finding
+    // the GCD. First, if B == 0 then the GCD would be
+    // the first number. Otherwise  A % B will solve it
     public int calculateGCD(int numA, int numB)
     {
         if (numB == 0){
@@ -15,6 +22,17 @@ public class SolutionGCD
         }
         return calculateGCD(numB, numA % numB);
     }
+
+    // For the main method you need to set a variable (calc)
+    // up within solutionGCD class to run calculateGCD
+    // You also need to import the scanner,
+    // java.util.Scanner to make sure you can have the user
+    // enter any numbers they want to solve for
+    // assign each of the user entries to the numA and numB
+    // The final print line has your variable (calc) with the
+    // calculateGCD method running its result and returning
+    // the proper answer. Viola!
+
 
     public static void main(String[] args) {
         SolutionGCD calc = new SolutionGCD();
